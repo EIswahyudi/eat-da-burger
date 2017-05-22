b7043f79d92a4d27accea30eb0bdc54fcf9b146d
Cary Cooper <Cary Cooper>
2017-05-22 02:49:37 -0400

    HEROKu DEPLOY

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,15 @@
 var mysql = require('mysql');
+var connection;
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: 'localhost',
-  user: 'root',
-  password:"password",
-  database: "burgers_db"
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password:"password",
+    database: "burgers_db"
+  });
 });
 
 // Create connection
